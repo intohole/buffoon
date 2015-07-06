ffbcb43bbc120359eb0c79e4504fd4f963719cf3
intohole <intoblack86@gmail.com>
2015-07-06 22:23:43 +0800

    联系添加路由设置

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/**
+    模块依赖
+*/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,16 +8,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//路由设置
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+//express 实例化
 var app = express();
 
-// view engine setup
+// 变量设置
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// 路由的 port设置
+app.set('port' , process.env.PORT || 3000 );
+
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,20 +31,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+routes(app) ;
+/*
+    设置路由
+    app.use('/', routes);
+    app.use('/users', users);
+*/
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +53,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -56,5 +61,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+//设置路由
 module.exports = app;
